31ccb9d520f689497071613c8dcaf16a32819340
OscarMaestre <profesor.oscar.gomez@gmail.com>
2016-09-21 11:28:00 +0200

    Scripts de inicio y cierre

---
+++ inicio.bat
@@ -0,0 +1,4 @@
+call "C:\Program Files (x86)\Mozilla Firefox\firefox.exe"
+call "C:\Program Files (x86)\ActiveState Komodo Edit 10\"
+cd C:\Users\ogomez\Documents\GitHub\lenguajes_marcas
+git pull
---
+++ reconstruir.bat
@@ -0,0 +1,9 @@
+call make singlehtml
+call make latex
+cd _build\latex
+call pdflatex ApuntesDeLenguajesDeMarcas.tex
+call pdflatex ApuntesDeLenguajesDeMarcas.tex
+cd ..
+cd ..
+call git commit -a --allow-empty-message -m ''
+call git push
