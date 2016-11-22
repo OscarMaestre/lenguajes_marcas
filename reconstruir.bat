817b867c5e1ae19a4ba67832f81617e7b814607b
Oscar G.G <profesor.oscar.gomez@gmail.com>
2016-11-22 19:21:16 +0100

    Incluida la copia

--- reconstruir.bat
+++ reconstruir.bat
@@ -1,9 +1,14 @@
 call make singlehtml
+copiar.py
 call make latex
 cd _build\latex
 call pdflatex ApuntesDeLenguajesDeMarcas.tex
 call pdflatex ApuntesDeLenguajesDeMarcas.tex
 cd ..
 cd ..
+call git commit -a --allow-empty-message -m ''
+call git push
+cd ..
+cd oscarmaestre.github.io
 call git commit -a --allow-empty-message -m ''
 call git push
