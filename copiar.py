d3608936a01a9adb68d213740eec594bd996f1aa
oscar <oscar@example.com>
2016-11-25 11:09:12 +0100

    Incluido

---
+++ copiar.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import os, shutil
+
+RUTA_DIRECTORIO_DESTINO ="docs"
+RUTA_APUNTES = "_build" + os.sep + "html"
+print ("Borrando " + RUTA_DIRECTORIO_DESTINO)
+shutil.rmtree ( RUTA_DIRECTORIO_DESTINO , ignore_errors=True)
+
+shutil.copytree(RUTA_APUNTES, RUTA_DIRECTORIO_DESTINO, ignore=None)
